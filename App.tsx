05ac080e14a271df047e37b261dbf435cc57248e
CesarCedillo97 <32661803+CesarCedillo97@users.noreply.github.com>
2024-12-18 02:21:42 -0700

    add linking config

--- App.tsx
+++ App.tsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import RootNavigator from './src/navigation/RootNavigator';
-import {StyleSheet} from 'react-native';
+import {Alert, Linking, StyleSheet} from 'react-native';
 import {AuthProvider} from './src/context/AuthContext';
 
 const App = () => {
+  useEffect(() => {
+    const handleInitialLink = async () => {
+      const initialUrl = await Linking.getInitialURL();
+      if (initialUrl) {
+        handleUrl(initialUrl);
+      }
+    };
+
+    const subscription = Linking.addEventListener('url', ({url}) => {
+      handleUrl(url);
+    });
+
+    handleInitialLink();
+
+    return () => {
+      subscription.remove();
+    };
+  }, []);
+
+  const handleUrl = (url: string) => {
+    const route = url.replace(/.*?:\/\//g, '');
+    const [path, id] = route.split('/');
+
+    if (path === 'open' && id) {
+      Alert.alert('Deeplink', `Abrir ruta: ${path}, con ID: ${id}`);
+    } else {
+      Alert.alert('Deeplink', `Ruta desconocida: ${route}`);
+    }
+  };
   return (
     <AuthProvider>
       <GestureHandlerRootView style={styles.container}>
